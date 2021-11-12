752d3c3b30b8860a0b4cbe67d1bc36bac31c9b72
Melanie Santos <1193380676@u.icesi.edu.co>
2021-11-12 17:36:07 -0500

    1.05

--- sketch.js
+++ sketch.js
@@ -17,22 +17,64 @@ class Fichas{
     circle(this.x,this.y,this.diameter)
 
   }
+
+
+
   moving(mx,my){
     this.x=mx;
     this.y=my;
   }
 
 
+getX(){
+  return this.x;
+}
+getY(){
+  return this.y;
+}
+
+
+
+}
+class Cuadrados{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.lado=150;
+    //rect(250+(200*i),150+(200*j),150);
+  }
+  createCuadrados(){
+    strokeWeight(2);
+    fill(255);
+    rect(this.x,this.y,this.lado);
+  }
+
+  validateCuadrados(vX,vY){
+    if (dist(this.x,this.y,vX,vY)<75) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  getX(){
+    return this.x;
+  }
+  getY(){
+    return this.y;
+  }
 
 }
 
 //0=X, 1=o
 let turn = 0;
 let maxTurn =0;
-let cuadrados = [];
-let cualquiera = 0;
+let cuadrado = [];
 let nuevaFichaR = [];
 let nuevaFichaZ = [];
+let turnFichaR = 0;
+let turnFichaZ = 0;
+
 
 
 function setup() {
@@ -45,33 +87,26 @@ function setup() {
     nuevaFichaZ.push(new Array)
   }
 
-  for (let index = 0; index < 3; index++) {
-    cuadrados.push(new Array(3));
-  }
-
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      cualquiera++
-      cuadrados[i][j] = cualquiera;
-      console.log(cuadrados);   
+      cuadrado.push(new Cuadrados (250+(200*i),150+(200*j)));
+       
      
     }
-    
   }
 }
 
 
 function draw() {
   background(220);
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      strokeWeight(2);
-      rect(250+(200*i),150+(200*j),150);
-    }
-  }
+  
+
+  cuadrado.forEach(cuadrado => {
+    cuadrado.createCuadrados();
+  })
    
     if (turn == 0){
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i <= turnFichaR; i++) {
         
         nuevaFichaR [i] = new Fichas (150,750);
         nuevaFichaR[i].beRect();
@@ -80,38 +115,46 @@ function draw() {
 
     
     } else if (turn == 1){
-      for (let i = 0; i < 4; i++) {
-        nuevaFichaZ [i] = new Fichas (150,750);
+      for (let i = 0; i < turnFichaZ; i++) {
         nuevaFichaZ[i].beZero();
+        nuevaFichaR [i] = new Fichas (150,750);
         
       }
-       
     }
-    
-  
-  if (turn == 0){
-    
-  } else if (turn == 1){
-    
-  }
-  
- 
-  
+}
+
+function mouseMoved(){
+
 }
 
 function mousePressed() {
 
   if (maxTurn < 9){
+    for (let i = 0; i < 9; i++) {
 
-    if (turn == 0){
-      turn = 1;
-      maxTurn++;
+      if (cuadrado[i].validateCuadrados(mouseX,mouseY)) {
       
-    } else if (turn == 1){
-      turn = 0;
-      maxTurn++;
+        if (turn == 0){
+          turn = 1;
+          maxTurn++;
+          nuevaFichaZ [turnFichaZ] = new Fichas (cuadrado[i].getX(),cuadrado[i].getY());
+          turnFichaZ++;
+
+          } else if (turn == 1){
+          turn = 0;
+          maxTurn++;
+          nuevaFichaR [turnFichaR] = new Fichas (cuadrado[i].getX(),cuadrado[i].getY());
+          turnFichaR++;
+      
+        }
+      }
     }
   }
+
+
+
+
+
   console.log(maxTurn);
 }
 
