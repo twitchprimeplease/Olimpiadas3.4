a659862d63000efa9165e36465dc1d67e607d5e9
Melanie Santos <1193380676@u.icesi.edu.co>
2021-11-12 18:50:24 -0500

    wHY

--- sketch.js
+++ sketch.js
@@ -1,4 +1,91 @@
-class Fichas{
+class Casilla{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.estado = 0;
+  }
+  pintar(){
+    switch(this.estado){
+      case 0:
+        fill(255);
+        break;
+        case 1:
+        fill(255,0,0);
+        break;
+        case 2:
+        fill(255,255,0);
+        break;
+    }
+    rect(this.x,this.y,100,100);
+  }
+
+  validar(mx, my){
+    if(dist(this.x+50, this.y+50, mx, my)<50){
+      return true;
+    }
+    return false;
+  }
+
+  cambiar(turno){
+    if(this.estado === 0){
+      this.estado = turno;
+      return true;
+    }
+    return false;
+  }
+
+}
+
+let turno = 1;
+let casillas = [];
+function setup() {  
+  createCanvas(300, 300);
+
+  let xAux = 0; 
+  let yAux = 0;
+  for(let i = 0; i < 9 ; i++){
+    casillas[i] = new Casilla(xAux, yAux);
+    xAux += 100;
+    if(xAux>=300){
+      xAux = 0;
+      yAux+=100;
+    }
+  }
+
+}
+
+function draw() {
+  background(220);
+  for(let i = 0; i < 9 ; i++){
+    casillas[i].pintar();
+  }
+}
+
+function mousePressed() {
+  for(let i = 0; i < 9 ; i++){
+    if(casillas[i].validar(mouseX, mouseY)){
+      if(casillas[i].cambiar(turno)){
+        turno = turno===1?2:1;
+        validarGanador();
+      }
+    }
+  }
+}
+
+function validarGanador() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+/*class Fichas{
   constructor(x,y){
     this.x=x;
     this.y=y;
@@ -80,11 +167,11 @@ let turnFichaZ = 0;
 function setup() {
   createCanvas(900, 900);
   for (let index = 0; index < 5; index++) {
-    nuevaFichaR.push(new Array)
+    nuevaFichaR.push(new Array())
   }
 
   for (let index = 0; index < 4; index++) {
-    nuevaFichaZ.push(new Array)
+    nuevaFichaZ.push(new Array())
   }
 
   for (let i = 0; i < 3; i++) {
@@ -116,8 +203,8 @@ function draw() {
     
     } else if (turn == 1){
       for (let i = 0; i < turnFichaZ; i++) {
-        nuevaFichaZ[i].beZero();
         nuevaFichaR [i] = new Fichas (150,750);
+        nuevaFichaZ[i].beZero();
         
       }
     }
@@ -157,4 +244,4 @@ function mousePressed() {
 
   console.log(maxTurn);
 }
-
+*/
